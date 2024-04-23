e823e5face7992b7c54853b0c4b844b9350d73e1
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-04-23 18:29:17 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-center p-24 ">
+      <span className="text-[#F67B49]">The website is under development.</span> The website will soon be live by 5 - 10 may.
+    </main>
+  );
+}
