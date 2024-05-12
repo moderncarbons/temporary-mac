83a9f803baa9a414db07bc531829178bf34c4d7c
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-05-12 13:38:38 +0530

    maintenance changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import logo from "../public/lightmode-logo.png";
 
 export default function Home() {
   return (
-    <main className="text-center p-24 ">
-      <span className="text-[#F67B49]">The website is under development.</span> The website will soon be live in 3 working days.
+    <main className="">
+      <Image src={logo} alt="logo" className="h-20 w-48 m-8" />
+      <p className="text-center p-24">
+        <span className="text-[#F67B49]">
+        The website is presently undergoing maintenance.
+        </span>{" "}
+        The website will soon be live.
+      </p>
     </main>
   );
 }
